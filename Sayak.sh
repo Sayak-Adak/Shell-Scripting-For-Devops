ba2f4120c951fd46ae5aa5ea6b79d5e11bc800cc
Ubuntu <ubuntu@ip-172-31-12-117.ap-south-1.compute.internal>
2025-06-20 20:42:24 +0000

    It is second code

---
+++ Sayak.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Hi I am Sayak Adak
+<< comment 
+I am BCA student and planning MCA.I want to become cloud solution architect with devopps engineer
+comment
+
+name="Sayak"
+
+echo "My name is $name"
